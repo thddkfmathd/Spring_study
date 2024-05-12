7f377f0a0e286112577d4b41896a6ff9dc69d973
thddkfmathd <thddkfmathd@kakao.com>
2024-05-13 04:27:31 +0900

    Feat : h2사용,  Jdbc repository 구현

--- inflearn/src/main/java/com/example/inflearn/SpringConfig.java
+++ inflearn/src/main/java/com/example/inflearn/SpringConfig.java
@@ -1,14 +1,24 @@
 package com.example.inflearn;
 
+import com.example.inflearn.repository.JdbcMemberRepository;
 import com.example.inflearn.repository.MemberRepository;
 import com.example.inflearn.repository.MemoryMemberRepository;
 import com.example.inflearn.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -16,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
